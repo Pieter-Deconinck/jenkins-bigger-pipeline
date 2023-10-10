01146aeeb824bceafc8bebc8c02ebadcc495810e
Pieter-Deconinck <100133263+Pieter-Deconinck@users.noreply.github.com>
2023-10-10 13:28:48 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -14,14 +14,12 @@ docker run -e "ACCEPT_EULA=Y" \
 
 # Pull github repo
 
-cd /home/vagrant
 
 git clone https://github.com/HoGentTIN/p3ops-demo-ap
 
 # Build .NET app
-dotnet restore /home/vagrant/p3ops-demo-app/src/Server/Server.csproj
+dotnet restore µp3ops-demo-app/src/Server/Server.csproj
 
 # Run .NET app
 dotnet run watch --project p3ops-demo-app/src/Server/Server.csproj
-
-
+proj
